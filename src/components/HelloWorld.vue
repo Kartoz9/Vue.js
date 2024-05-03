f77cef6ecacf7a72643befbf1e5cbfab22784227
Kartoz9 <gluhovski241@gmail.com>
2024-05-04 01:58:21 +0300

    Перший коміт: додано Vue.js додаток

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <p>Спеціальність: {{ speciality }}</p>
+    <p>Курс: {{ course }}</p>
+    <p>Група: {{ groupName }}</p>
+    <p>ПІБ: {{ fullName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      speciality: 'Інженерія програмного забезпечення',
+      course: 1,
+      groupName: '1ПРс',
+      fullName: 'Глуховський І.В.'
+    }
+  }
+}
+</script>
+
